13df2ff649b5c55a6b3efc7cd20d3cd18a6002b5
AhmetHanbay <90829959+AhmetHanbay@users.noreply.github.com>
2024-08-18 18:38:32 +0300

    C-sharp-degiskenler
    C# değişkenler dersi + Uygulama

---
+++ degiskenler/Program.cs
@@ -0,0 +1,15 @@
+﻿var kdvOrani = 1.18;
+var urunA = 5000;
+var urunB = 6000;
+var urunC = 7000;
+var urunD = 8000;
+
+
+Console.WriteLine(urunA * kdvOrani);
+Console.WriteLine(urunB * kdvOrani);
+Console.WriteLine(urunC * kdvOrani);
+Console.WriteLine(urunD * kdvOrani);
+
+//Değişken ismi tanımlanırken boşluk içermemeli
+//Değişken ismi tanımlanırken sayı ile başlanamaz
+//Değişken ismi tanımlanırken büyük küçük harf farkı vardır yani büyük harfle aynı isimde bir değişken tanımlarsak o farklı bir değişken gibi olur
---
+++ degiskenler/Uygulama/Uygulama-1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Öğrenci Bilgilerini içeren değişkenleri oluşturalım
+
+var ad = "Ali";
+var soyad = "Demir";
+var numara = 1234321;
+var cinsiyet = "Erkek";
+var tc_no = 12332112332;
+var dogum_yili = 1992;
+var adres = "İstanbul / Türkiye";
+var yas = 32;
+
+
+Console.WriteLine("Öğrenci ad : " + ad);
+Console.WriteLine("Öğrenci Soyad : " + soyad);
+Console.WriteLine("Öğrenci numara : " + numara);
+Console.WriteLine("Öğrenci cinsiyet : " + cinsiyet);
+Console.WriteLine("Öğrenci TC no : " + tc_no);
+Console.WriteLine("Öğrenci doğum yılı : " + dogum_yili);
+Console.WriteLine("Öğrenci adresi : " + adres);
+Console.WriteLine("Öğrenci yaşı : " + yas);
+
+//Ürünlerin toplam fiyatlarını hesaplayalım
+
+var urun1_fiyat = 100;
+var urun2_fiyat = 250;
+var urun3_fiyat = 150;
+var toplam = urun1_fiyat + urun2_fiyat + urun3_fiyat;
+
+Console.WriteLine("Ürünlerin toplam fiyatı :" + toplam);
+
+
+
